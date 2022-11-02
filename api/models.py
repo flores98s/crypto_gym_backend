d634849b78d20bcfe8fb03569083c8639956646e
flores98s <saul.flores1@ujcv.edu.hn>
2022-11-01 22:11:18 -0600

    Se agrego el modelo de TipoMembresia con sus migraciones

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class TipoMembresia(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.IntegerField()
+    descripcion = models.CharField(max_length=100)
+    
