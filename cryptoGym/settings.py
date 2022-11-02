b66e3b0efd26910289b6167dbe7f12cfa7f69e91
flores98s <saul.flores1@ujcv.edu.hn>
2022-11-01 21:30:56 -0600

    Cambio de base de datos a Postgresql

--- cryptoGym/settings.py
+++ cryptoGym/settings.py
@@ -75,9 +75,15 @@ WSGI_APPLICATION = 'cryptoGym.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'gFdqNE1G2MDIaWm9ZSWf',
+        'HOST': 'containers-us-west-86.railway.app',
+        'PORT': 6903,
     }
+
+   
 }
 
 
