7388ee2363e1a42676f535a941d7d09d2f601226
emerson <emersonardon1@gmail.com>
2022-11-01 22:37:06 -0500

    cambio

--- cryptoGym/settings.py
+++ cryptoGym/settings.py
@@ -82,10 +82,11 @@ DATABASES = {
         'HOST': 'containers-us-west-86.railway.app',
         'PORT': 6903,
     }
-
+    
    
 }
 
+a=1
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
