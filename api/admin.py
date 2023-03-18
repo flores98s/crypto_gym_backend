f4e243e4de6ef7a5895076870aa629cdc36f5d4b
flores98s <75622716+flores98s@users.noreply.github.com>
2023-03-18 00:07:03 +0000

    Se agregaron permisos en el admin

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.models import Permission
+
+
 
 
 # Register your models here.
@@ -49,6 +52,8 @@ admin.site.register(Compra)
 admin.site.register(Administrador)
 admin.site.register(LoginAdministrador)
 admin.site.register(MusculoEjercicio)
+
+admin.site.register(Permission)
 
 
 
